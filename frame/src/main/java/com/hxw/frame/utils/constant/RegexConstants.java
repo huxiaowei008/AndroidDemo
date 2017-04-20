b1ea99cf201b4245917885376674429c321a2dda
huxiaowei008 <huxiaowei008@126.com>
2017-04-20 17:01:21 +0800

    增加时间和spannable工具类

--- frame/src/main/java/com/hxw/frame/utils/constant/RegexConstants.java
+++ frame/src/main/java/com/hxw/frame/utils/constant/RegexConstants.java
@@ -1,63 +1,11 @@
-package com.hxw.frame.utils;
+package com.hxw.frame.utils.constant;
 
 /**
- * 常量相关工具类
- * Created by hxw on 2017/2/16.
+ * 正则相关常量
+ * Created by hxw on 2017/4/9.
  */
 
-public class ConstUtils {
-    private ConstUtils() {
-        throw new UnsupportedOperationException("u can't instantiate me...");
-    }
-
-    /******************** 存储相关常量 ********************/
-    /**
-     * KB与Byte的倍数
-     */
-    public static final int KB = 1024;
-    /**
-     * MB与Byte的倍数
-     */
-    public static final int MB = 1048576;
-    /**
-     * GB与Byte的倍数
-     */
-    public static final int GB = 1073741824;
-
-    public enum MemoryUnit {
-        BYTE,
-        KB,
-        MB,
-        GB
-    }
-
-    /******************** 时间相关常量 ********************/
-    /**
-     * 秒与毫秒的倍数
-     */
-    public static final int SEC = 1000;
-    /**
-     * 分与毫秒的倍数
-     */
-    public static final int MIN = 60000;
-    /**
-     * 时与毫秒的倍数
-     */
-    public static final int HOUR = 3600000;
-    /**
-     * 天与毫秒的倍数
-     */
-    public static final int DAY = 86400000;
-
-    public enum TimeUnit {
-        MSEC,
-        SEC,
-        MIN,
-        HOUR,
-        DAY
-    }
-
-    /******************** 正则相关常量 ********************/
+public final class RegexConstants {
     /**
      * 正则：手机号（简单）
      */
@@ -155,5 +103,4 @@ public class ConstUtils {
     public static final String REGEX_NEGATIVE_FLOAT = "^-[1-9]\\d*\\.\\d*|-0\\.\\d*[1-9]\\d*$";
 
     /************** If u want more please visit http://toutiao.com/i6231678548520731137/ **************/
-
 }
