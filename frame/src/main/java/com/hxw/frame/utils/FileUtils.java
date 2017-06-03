a69b078969e93e8baa3e59e3ffe8f39940ee12bd
huxiaowei008 <huxiaowei008@126.com>
2017-06-03 17:14:38 +0800

    增加加密工具

--- frame/src/main/java/com/hxw/frame/utils/FileUtils.java
+++ frame/src/main/java/com/hxw/frame/utils/FileUtils.java
@@ -113,19 +113,4 @@ public class FileUtils {
         return filePath.substring(lastPoi + 1);
     }
 
-    /**
-     * 关闭一些能关闭的东西
-     *
-     * @param closeable
-     */
-    public static void closeQuietly(Closeable closeable) {
-        if (closeable != null) {
-            try {
-                closeable.close();
-            } catch (RuntimeException rethrown) {
-                throw rethrown;
-            } catch (Exception ignored) {
-            }
-        }
-    }
 }
