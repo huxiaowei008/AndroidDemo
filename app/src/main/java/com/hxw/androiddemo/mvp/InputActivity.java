524b0bbb2046cda8fd1d427319e37b20ed3fcc76
huxiaowei <huxiaowei008@126.com>
2017-12-13 19:23:13 +0800

    使用inputView

--- app/src/main/java/com/hxw/androiddemo/mvp/InputActivity.java
+++ app/src/main/java/com/hxw/androiddemo/mvp/InputActivity.java
@@ -3,12 +3,11 @@ package com.hxw.androiddemo.mvp;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 import com.hxw.androiddemo.R;
 import com.hxw.frame.base.BaseActivity;
 import com.hxw.frame.di.AppComponent;
-import com.hxw.frame.widget.InputView;
+import com.hxw.input.InputView;
 
 import butterknife.BindView;
 
@@ -18,12 +17,16 @@ import butterknife.BindView;
  */
 
 public class InputActivity extends BaseActivity {
-    @BindView(R.id.input)
-    InputView inputView;
-    @BindView(R.id.edt_max)
-    EditText edtMax;
-    @BindView(R.id.btn_sure)
-    Button btn;
+
+
+    @BindView(R.id.input1)
+    InputView input1;
+    @BindView(R.id.input2)
+    InputView input2;
+    @BindView(R.id.btn1)
+    Button btn1;
+    @BindView(R.id.btn2)
+    Button btn2;
 
     @Override
     public int getLayoutId() {
@@ -37,19 +40,22 @@ public class InputActivity extends BaseActivity {
 
     @Override
     public void init(Bundle savedInstanceState) {
-        btn.setOnClickListener(new View.OnClickListener() {
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                int max = Integer.valueOf(edtMax.getText().toString());
-//                if (max > 0) {
-//                    inputView.setMaxLength(max);
-//                }
-                inputView.setText("adfasd");
+                input1.setMaxLength(8);
+                input2.setMaxLength(8);
             }
         });
 
-
-
-
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                input1.setMaxLength(6);
+                input2.setMaxLength(6);
+            }
+        });
     }
+
+
 }
